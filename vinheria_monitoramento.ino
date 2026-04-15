4cff8d6b90724117e07680b7d8db165a8156719e
liviapisanello <124214519+liviapisanello@users.noreply.github.com>
2026-04-15 20:48:08 -0300

    Refactor LDR threshold values and LED logic

--- vinheria_monitoramento.ino
+++ vinheria_monitoramento.ino
@@ -1,50 +1,87 @@
-int ldr = A5;
+int ldr = A0;
+ 
 int ledVerde = 9;
+ 
 int ledAmarelo = 6;
+ 
 int ledVermelho = 11;
+ 
 int buzzer = 10;
+ 
 int valorLdr = 0;
-
+ 
 void setup()
+ 
 {
+ 
   pinMode(ledVerde, OUTPUT);
+ 
   pinMode(ledAmarelo, OUTPUT);
+ 
   pinMode(ledVermelho, OUTPUT);
+ 
   pinMode(buzzer, OUTPUT);
+ 
   Serial.begin(9600);
+ 
 }
-
+ 
 void loop()
+ 
 {
+ 
   valorLdr = analogRead(ldr);
+ 
   Serial.println(valorLdr);
-
-  if (valorLdr >= 750)
+ 
+  if (valorLdr > 800)
+ 
   {
-    digitalWrite(ledVerde, HIGH);
+ digitalWrite(ledVerde, LOW);
+ 
     digitalWrite(ledAmarelo, LOW);
-    digitalWrite(ledVermelho, LOW);
-    digitalWrite(buzzer, LOW);
-  }
-
-  else if (valorLdr >= 200)
-  {
-    digitalWrite(ledVerde, LOW);
-    digitalWrite(ledAmarelo, HIGH);
-    digitalWrite(ledVermelho, LOW);
-    digitalWrite(buzzer, LOW);
-  }
-
-  else
-  {
-    digitalWrite(ledVerde, LOW);
-    digitalWrite(ledAmarelo, LOW);
+ 
     digitalWrite(ledVermelho, HIGH);
+ 
     digitalWrite(buzzer, HIGH);
-    delay(3000); 
+ 
+    delay(3000);
+ 
     digitalWrite(buzzer, LOW);
-    delay(1000); 
+ 
+    delay(1000);
+ 
+ 
   }
-
+ 
+  else if (valorLdr > 600)
+ 
+  {
+ 
+    digitalWrite(ledVerde, LOW);
+ 
+    digitalWrite(ledAmarelo, HIGH);
+ 
+    digitalWrite(ledVermelho, LOW);
+ 
+    digitalWrite(buzzer, LOW);
+ 
+  }
+ 
+  else
+ 
+  {
+ 
+   
+   digitalWrite(ledVerde, HIGH);
+ 
+    digitalWrite(ledAmarelo, LOW);
+ 
+    digitalWrite(ledVermelho, LOW);
+ 
+    digitalWrite(buzzer, LOW);
+  }
+ 
   delay(200);
+ 
 }
